405b49b72e8cc6b091d70c82eaa32ab1b157beec
Lugard Agu <agulugard@gmail.com>
2023-10-19 20:46:30 +0100

    Set time interval based on new date

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 const Random_Quote_API_URL = 'https://api.quotable.io/random';
 const quoteDisplayElement = document.getElementById('quoteDisplay');
 const quoteInputElement = document.getElementById('quoteInput');
+const timerElement = document.getElementById('timer');
+
 
 quoteInputElement.addEventListener('input', () => {
   const arrayQuote = quoteDisplayElement.querySelectorAll('span');
   const arrayValue = quoteInputElement.value.split('')
+  let correct = true;
   arrayQuote.forEach((characterSpan, index) => {
     const character = arrayValue[index]
     if (character == null) {
       characterSpan.classList.remove('correct');
       characterSpan.classList.remove('incorrect');
+      correct = false;
     }else if (character === characterSpan.innerText) {
       characterSpan.classList.add('correct')
       characterSpan.classList.remove('incorrect');
     }else {
       characterSpan.classList.remove('correct');
       characterSpan.classList.add('incorrect')
+      correct = false;
     }
   })
+  if (correct) renderNewQuote()
 })
 
 function getRandomQuote() {
@@ -35,6 +41,20 @@ async function renderNewQuote() {
     quoteDisplayElement.appendChild(characterSpan);
   })
   quoteInputElement.value = null;
+  startTimer();
+}
+
+let startTime;
+function startTimer() {
+  timerElement.innerText = 0;
+  startTime = new Date();
+  setInterval(() => {
+    timerElement.innerText = getTimerTime()
+  }, 1000)
+}
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000);
 }
 
 renderNewQuote();
