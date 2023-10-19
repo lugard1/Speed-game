12d98d3d060e4d5c5b42ae6c2e1c85ca83fbeb26
Lugard Agu <agulugard@gmail.com>
2023-10-19 19:51:49 +0100

    Implement text comparator

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ const Random_Quote_API_URL = 'https://api.quotable.io/random';
 const quoteDisplayElement = document.getElementById('quoteDisplay');
 const quoteInputElement = document.getElementById('quoteInput');
 
+quoteInputElement.addEventListener('input', () => {
+  const arrayQuote = quoteDisplayElement.querySelectorAll('span');
+  const arrayValue = quoteInputElement.value.split('')
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index]
+    if (character == null) {
+      characterSpan.classList.remove('correct');
+      characterSpan.classList.remove('incorrect');
+    }else if (character === characterSpan.innerText) {
+      characterSpan.classList.add('correct')
+      characterSpan.classList.remove('incorrect');
+    }else {
+      characterSpan.classList.remove('correct');
+      characterSpan.classList.add('incorrect')
+    }
+  })
+})
+
 function getRandomQuote() {
   return fetch(Random_Quote_API_URL)
   .then(response => response.json())
@@ -10,7 +28,12 @@ function getRandomQuote() {
 
 async function renderNewQuote() {
   const quote = await getRandomQuote();
-  quoteDisplayElement.innerText = quote;
+  quoteDisplayElement.innerHTML = '';
+  quote.split('').forEach(character => {
+    const characterSpan = document.createElement('span');
+    characterSpan.innerText =character;
+    quoteDisplayElement.appendChild(characterSpan);
+  })
   quoteInputElement.value = null;
 }
 
